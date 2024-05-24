f994f0d436540e52a1c1a184ca33f5ee2ce053b6
UniAllYuki <isaacbrindes41@gmail.com>
2024-05-24 11:05:34 -0300

    Primeira explicação sobre Herança

---
+++ herancaEPolimorfismo/herancaAulaUm/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Herança
+		/* Tipo de associação que permite que uma classe herde todos dados e
+		 * comportamento de outra
+		 * Vantagens: Reuso; Polimorfismo
+		 * Sintaxe: class A extends B
+		 */
+		// Exemplo de uso da Herança: Conta normal e Conta Empresárial. (Nas classes)
+		
+		/* Definições importantes:
+		 * Relação "é-um"
+		 * Genealização/especialização
+		 * Superclasse (classe base) / subclasse (classe derivada)
+		 * Herançã / extensão
+		 *  Herança é uma associação entre classes (e não entre objetos)
+		 */
+		
+	}
+
+}
