5d89871fa8f21e170c8c408c889083d349e94c38
UniAllYuki <isaacbrindes41@gmail.com>
2024-05-28 12:17:47 -0300

    Exceções Personalizadas - Método Muito Ruim

---
+++ tratamentoDeExcecoes/excecoesPersonalizadas/metodoMuitoRuim/application/Main.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+		
+		// Criando Exceções Personalizadas		// Problema Exemplo;
+		/* Fazer um programa para ler os dados de uma reserva de hotel (numéro do quarto, data
+		 * de entrega e data de saída) e mostrar os dados da reserva, inclusive sua duranção em
+		 * dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+		 * novamente a reserva com os dados atualizados. O Programa não deve aceitar dados
+		 * inválidos para a reserva, conforme as regras a seguir:
+		 * - Alterações de reserva só podem ocorrer para datas futuras.
+		 * - A Data de saída deve ser maior que a data de entrada.
+		 */
+		
+		// Solução "Muito-Ruim"
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Room Number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in Date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		// sdf.parse gera uma exceção, por isso precisa da declaração de throws ParseException.
+		System.out.print("Check-Out Date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error in Reservation: Check-out date must be after check-in date!");
+			System.exit(0);
+		}
+		
+		Reservation res = new Reservation(number, checkIn, checkOut);
+		System.out.println("Reservation: " + res);
+		
+		System.out.println();
+		System.out.println("Enter data to update the reservation: ");
+		System.out.print("Check-in Date (dd/MM/yyyy): ");
+		checkIn = sdf.parse(sc.next());
+		System.out.print("Check-Out Date (dd/MM/yyyy): ");
+		checkOut = sdf.parse(sc.next());
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			System.out.println("Error in reservation: Reservation dates for update must be future dates!");
+			System.exit(0);
+		} else if (!checkOut.after(checkIn)) {
+			System.out.println("Error in Reservation: Check-out date must be after check-in date!");
+			System.exit(0);
+		}
+		
+		res.updateDates(checkIn, checkOut);
+		System.out.println("Reservation: " + res);
+		
+		sc.close();
+	}		
+	
+	// Essa é a versão muito Ruim do projeto, muito ruim por que as exceções estão no programa principal.
+	
+} 			
